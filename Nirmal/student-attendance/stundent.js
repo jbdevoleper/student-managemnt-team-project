2a1bbd771a4ec626347814d28ec0dbe71ce244d6
nirmal0304 <suthaya858@gmail.com>
2026-02-08 14:56:14 +0530

    students

---
+++ Nirmal/student-attendance/stundent.js
@@ -0,0 +1,9 @@
+let students =[
+    {name:"Alex johnson",id:"5035",status:"Present"},
+    {name:"Alex ",id:"5036",status:"Present"},
+    {name:"johnson",id:"5037",status:"Present"},
+    {name:"john",id:"5038",status:"Present"},
+    {name:"jebin",id:"5039",status:"Present"}
+];
+let currentPage=1;
+let rowsPerPage=5;
