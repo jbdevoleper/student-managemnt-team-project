db06778421ff0dbfc781e82604a7f7a394ef19b0
jbdevoleper <jebinvasanth22@gmail.com>
2026-02-09 11:24:41 +0530

    jb

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -64,3 +64,17 @@ function updateDashboard(){
 }
 
 updateDashboard();
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link=>{
+    link.addEventListener("click",()=>{
+        navLinks.forEach(l=>{
+            l.classList.remove("active")
+        });
+        link.classList.add("active");
+    });
+    link.addEventListener("click",(e)=>{
+        e.preventDefault();
+    })
+})
